f97527e38d055bec43e9410a6612ee7c876000a1
Jessy <1973008@maranatha.ac.id>
2020-07-12 22:49:34 +0700

    Update 12 Juli 2020

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create!(email: 'admin@example.com',
+User.create!(email:'admin@example.com',
     password: 'password',
     password_confirmation: 'password',
     admin: true)
 
+
 25.times do |i|
-    Product.create!(
-        name: "Pizza",
-        description "Lorem ipsum bbq chicken upstate newyork free range dough",
-        price: '11.99',
-        catering: true
-    )
+Product.create!(
+name: "Pizza",
+description: "Lorem ipsum bbq chicken upstate newyork free range dough",
+price: 11.99,
+catering: true
+)
 end
